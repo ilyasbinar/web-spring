b14732f30e622e436c11300c976a028b0bcc85eb
birruby <ilyas@lua.co.id>
2023-11-23 17:06:26 +0700

    keren

--- src/main/java/com/example/webspring/controller/HomeController.java
+++ src/main/java/com/example/webspring/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 
     @RequestMapping("/")
     public List<String> greeting(){
-        return List.of("Hallo", "Binar Keren banget");
+        return List.of("Hallo", "Binar Keren aja");
     }
 }
